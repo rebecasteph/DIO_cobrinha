b6d86eee226d865cf0c98bc4b902160590f5ff27
rebecasteph <rsva.lic@uea.edu.br>
2021-04-17 03:46:45 -0400

    estilo da página

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function criarCobrinha(){
 }
 
 function drawFood(){
-    context.fillStyle = "red";
+    context.fillStyle = "#D5057F";
     context.fillRect(food.x, food.y, box, box);
 }
 
@@ -46,6 +46,14 @@ function iniciarJogo(){
     if (snake[0].x < 0        && direction == "left")   snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down")   snake[0].y = 0;
     if (snake[0].y < 0        && direction == "up")     snake[0].y = 16 * box;
+
+    for (let i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game over :/");
+        }
+        
+    }
 
     criarBG();
     criarCobrinha();
